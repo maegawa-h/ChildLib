5ae1e9719f676b9f09a2a0ae30d1852cd49d8abc
maegawa_h <hiroshi.maegawa.j6c@jpgr.denso.com>
2022-11-25 17:37:51 +0900

    initial commit

---
+++ src/ChildLib/ChildLib/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using GrandChildLib;
+
+namespace ChildLib
+{
+    public class Child
+    {
+        private GrandChild grand;
+
+        public Child()
+        {
+            grand = new GrandChild();
+        }
+
+        public void Call()
+        {
+            Console.WriteLine($"Child.Call(){Assembly.GetExecutingAssembly().GetName().Version}");
+
+            grand.Call();
+        }
+    }
+}
